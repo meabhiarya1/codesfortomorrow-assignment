a209d0630bb03fe235c249fc21dbb000505ca7b8
Abhishek kumar <abhi.arya97161@gmail.com>
2025-05-17 11:21:50 +0530

    feat: update Login component with enhanced UI and functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import Login from "./Comp/Login";
 
 function App() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+    <div className="  ">
       <Login />
     </div>
   );
